4b8400893f2a3c6e87f663214c32741948f6245b
Edwin Dalorzo <edwin@dalorzo.com>
2014-08-19 10:13:06 -0600

    Reminder to demonstrate automatic service layer validation.

--- src/main/java/com/backcountry/web/services/CustomerService.java
+++ src/main/java/com/backcountry/web/services/CustomerService.java
@@ -2,13 +2,15 @@ package com.backcountry.web.services;
 
 import com.backcountry.web.model.CustomerModel;
 import com.backcountry.web.services.execptions.ServiceException;
+import org.springframework.validation.annotation.Validated;
 
-/**
- * @author Edwin Dalorzo.
- */
+import javax.validation.Valid;
+
+//demo automatic service validation
+//@Validated
 public interface CustomerService {
 
-    public CustomerModel createCustomer(CustomerModel model) throws ServiceException;
+    public CustomerModel createCustomer(/*@Valid*/ CustomerModel model) throws ServiceException;
     public CustomerModel getCustomerById(Integer id) throws ServiceException;
 
 }
